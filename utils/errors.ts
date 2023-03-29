765b77ef00c747a1633fc9aebf0e103df2d07bed
git-asia <tuszynska.joanna@gmail.com>
2023-03-29 19:23:06 +0200

    Added and tested global error handling.

--- utils/errors.ts
+++ utils/errors.ts
@@ -3,12 +3,12 @@ import {NextFunction, Request, Response} from "express";
 export class ValidationError extends Error {
 }
 
-export const handleError = (err: Error, req: Request, res: Response) => {
+export const handleError = (err: Error, req: Request, res: Response, next: NextFunction) => {
     console.error(err);
 
     res
         .status(err instanceof ValidationError ? 400 : 500)
         .json({
-            message: 'Sorry, please try again later.',
+            message: err instanceof ValidationError ? err.message : 'Sorry, please try again later.',
         });
 }
