48d22565fd66f1f83b991909f43790a3bd574f5b
git-asia <tuszynska.joanna@gmail.com>
2023-03-29 16:25:49 +0200

    Added json handling.

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
-import express from "express";
-import cors from "cors";
-import "express-async-errors";
+import express, {json} from 'express';
+import cors from 'cors';
+import 'express-async-errors';
 
 const app = express();
 
 app.use(cors( {
     origin: 'http://localhost:3000',
 }));
+
+app.use(json());
 
 app.listen(3002, '0.0.0.0', () => {
     console.log('Listening on port http://localhost:3002');
